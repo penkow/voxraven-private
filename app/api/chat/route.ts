43ef11e6a2ba14d61b725fab4eb18dbf466b19f9
Nikolay Penkov <n.penkow@gmail.com>
2025-05-23 09:44:21 +0300

    video page work

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -9,8 +9,7 @@ export async function POST(req: Request) {
 
   const openAiProvider = createOpenAI({
     apiKey:
-      "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",
-  });
+ });
 
   const model = openAiProvider.languageModel("gpt-4.1-nano");
 
